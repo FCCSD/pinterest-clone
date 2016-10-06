669a8044cd078419dee142d7d576f2ab22cbd80d
Zoe Kumagai <zoekumagai@Zoe-Kumagais-MacBook-2.local>
2016-10-06 15:06:16 -0700

    Added front end routing with react-router

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
 import './styles/css/main.css';
 
+import AddWrapper from './components/AddTile/AddWrapper.jsx';
 import BoardWrapper from './components/Board/BoardWrapper.jsx';
 import NavBar from './components/Nav/NavBar.jsx';
+import LoginWrapper from './components/Login/LoginWrapper.jsx';
+import UserWrapper from './components/User/UserWrapper.jsx';
 
-let PageWrapper = function() {
+function PageWrapper(props) {
   return (
     <div className="page-wrapper">
       <NavBar />
-      <BoardWrapper />
+      {props.children}
     </div>
   )
 }
 
-ReactDOM.render(
-  <PageWrapper />
-  ,
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={PageWrapper}>
+      <Route path='/home' component={BoardWrapper} />
+      <Route path='/user' component={UserWrapper}/>
+      <Route path='/login' component={LoginWrapper} />
+      <Route path='/add' component={AddWrapper} />
+    </Route>
+  </Router>
+  ),
   document.getElementById('root')
 );
