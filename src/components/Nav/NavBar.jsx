f480c563ec8f4a17c5e347f9cb187d57b29c26bc
Zoe Kumagai <zoekumagai@Zoe-Kumagais-MacBook-2.local>
2016-10-06 15:14:16 -0700

    Added react-router links to nav icons

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import UserStore from './../../stores/UserStore/UserStore.js';
+import { Link } from 'react-router';
 
 export default class NavBar extends React.Component {
   constructor() {
@@ -10,7 +12,7 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div className="nav-bar">
-        <div className="nav-logo"><i className="fa fa-pinterest fa-2x"></i></div>
+        <div className="nav-logo"><Link to="/home"><i className="fa fa-pinterest fa-2x"></i></Link></div>
         <div className="nav-search">
           <i className="fa fa-search"></i>
           <input type="text" />
@@ -18,7 +20,7 @@ export default class NavBar extends React.Component {
         <div className="nav-menu">
           <span></span>
         </div>
-        <div className="nav-user"><i className="fa fa-user fa-2x"></i></div>
+        <div className="nav-user"><Link to="/user"><i className="fa fa-user fa-2x"></i></Link></div>
         <div className="nav-notifications"><i className="fa fa-bell fa-2x"></i></div>
       </div>
     );
